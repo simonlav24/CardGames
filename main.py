6e1283ee903cc091b4f92346a1adeedcf360e7ed
simon Lab <simelav24@gmail.com>
2025-10-07 22:49:16 +0300

    doubleclick bug fixed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 
 import pygame
+from pygame.math import Vector2
 
 import utils.custom_random as custom_random
 import game_globals
@@ -27,12 +28,14 @@ def main():
     clock = pygame.time.Clock()
     FPS = 60
     DOUBLE_CLICK_INTERVAL = 400
+    DOUBLE_CLICK_OFFSET_SQUARED = 25
 
-    game = KlondikeGame()
+    game = ShitheadGame()
     cards = game.setup_game()
 
     # Main loop
     last_click_time = 0
+    last_click_pos = (0,0)
 
     done = False
     while not done:
@@ -48,10 +51,15 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Left click
                     current_time = pygame.time.get_ticks()
-                    if current_time - last_click_time < DOUBLE_CLICK_INTERVAL:
+                    current_pos = event.pos
+                    if (
+                        current_time - last_click_time < DOUBLE_CLICK_INTERVAL and
+                        Vector2(current_pos).distance_squared_to(last_click_pos) < DOUBLE_CLICK_OFFSET_SQUARED
+                    ):
                         game.on_mouse_double_click(event.pos)
                     game.on_mouse_press(event.pos)
                     last_click_time = current_time
+                    last_click_pos = event.pos
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:  # Left click
                     game.on_mouse_release(event.pos)
